697d4fae7bbe65634f61f97c24250b6c23c7d0f9
Ahmed Nawar <ahmedmnawar@gmail.com>
2014-03-31 01:10:38 +0200

    removed old commented out code

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ var db = require('./models/db_operations.js');
 var userRegistry = require('./models/users_registry.js');
 var app = express();
 
-
 // all environments
 app.set('port', process.env.PORT || 3000);
 app.use(express.logger('dev'));
@@ -19,33 +18,12 @@ app.use(app.router);
 
 app.post('/listen', routes.listen.listenToMusic);
 app.post('/follow', routes.follow.followUser);
-app.get('/show', function(req, res) {
-	userRegistry.showAll();
-	res.end(200);
-});
+app.get('/recommendations', routes.recommendationEngine.recommendMusic);
 
-/*app.post('/follow', function(req, res) {
-	var b = req.body;
-	var from = b.from, to = b.to;
-	mydata.followUser(from, to);
-	res.send(200);
-
-});
-
-app.get('/recommendations:user', function (req, res) {
-	var user = req.params.user;
-	var arr = mydata.recommendMusic(user);
-	res.setHeader('Content-Type', 'application/json');
-	var s = {"list":  arr};
-	var str = JSON.stringify(s);
-	console.log(str);
-	res.end(str);
-})
-*/
 
 var startServer = function () {
-	app.server = http.createServer(app);
-	app.server.listen(3000);
+    app.server = http.createServer(app);
+    app.server.listen(3000);
 }
 
 db.openDB('axiomZen', startServer);
