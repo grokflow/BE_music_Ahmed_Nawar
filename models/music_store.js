03b14aac1318ed441f2657787bbcd0b58a8e862f
Ahmed Nawar <ahmedmnawar@gmail.com>
2014-03-31 01:11:40 +0200

    added a getAllMusic function

--- models/music_store.js
+++ models/music_store.js
@@ -1,14 +1,24 @@
 var file = './data/musics.json',
-	musicFile = require('fs').readFileSync(file, 'utf8'),
-	musicStore,
-	getGenresForMusic;
-	;
+    musicFile = require('fs').readFileSync(file, 'utf8'),
+    musicStore,
+    getGenresForMusic;
+    ;
 
 musicStore = JSON.parse(musicFile);
 
 getTagsFor = function(music_title) {
-	return musicStore[music_title];
+    return musicStore[music_title];
+}
+
+getAllMusic = function() {
+	var musicList = [];
+	for (var music in musicStore) {
+  		if (musicStore.hasOwnProperty(music)) {
+    		musicList.push(music);
+  		}
+  	}
+  	return musicList;
 }
 
 module.exports.getTagsFor = getTagsFor;
-
+module.exports.getAllMusic = getAllMusic;
