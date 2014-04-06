f6e015164033f053c0af41f77db4639363ca4e80
Ahmed Nawar <ahmedmnawar@gmail.com>
2014-04-07 01:37:39 +0200

    added validation and changed the getUser logic
    accommodated the getUser requests to comply with the new interface

--- routes/follow.js
+++ routes/follow.js
@@ -1,33 +1,29 @@
+/*
+    This route defines the POST/follow endpoint. 
+*/
 var db = require('../models/db_operations.js');
 var follow;
 var followUser;
 var userRegistry = require('../models/users_registry.js');
+var validation = require('../utils/input-validator.js');
 
+//the endpoint for the POST request
 followUser = function(req, res) {
         var from = req.body.from, to = req.body.to;
-        follow(from, to);
-        res.send(200);
+        if (!validation.checkInput(from, 'userID') || !validation.checkInput(to, 'userID'))
+            res.send(403, 'Forbidden user format');
+
+        follow(from, to, function() { 
+            res.send(200);
+        });
 }
 
-follow = function (from_userID, to_userID) {
-    var fromUser = userRegistry.getUser(from_userID);
-    var toUser =  userRegistry.getUser(to_userID);
-    if (fromUser && toUser) {
-        registerFollowee(fromUser);
-        return;
-    }
+// registering the follow relationship between 2 users to the user registry and database
+follow = function (from_userID, to_userID, callback) {
+    var fromUser, toUser;
+    userRegistry.getUser(from_userID, registerFollowee);
+    userRegistry.getUser(to_userID, registerFollowee);
     
-    if (fromUser === undefined) {
-        db.getFromDB(from_userID, registerFollowee);
-    } else {
-        registerFollowee(fromUser);
-    }
-    if (toUser === undefined) {
-        db.getFromDB(to_userID, registerFollowee);
-    } else {
-        registerFollowee(toUser);
-    }
-
     function registerFollowee (user) {
         if (user.userId === from_userID)
             fromUser = user;
@@ -37,6 +33,7 @@ follow = function (from_userID, to_userID) {
         if (fromUser && toUser) {
             fromUser.addFollowee(to_userID);
             db.addToArrayInDB(from_userID, 'followees', to_userID);
+            callback();
         }
     }        
 }
