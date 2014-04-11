55d9ca0a19b6f3c6a287a3315e8bf2273669dbc7
Ahmed Nawar <ahmedmnawar@gmail.com>
2014-04-11 16:39:36 +0200

    used async to control flow

--- test/app.test.js
+++ test/app.test.js
@@ -1,86 +1,79 @@
+var async = require('async');
 var fs = require('fs');
 var request = require('supertest');
 var serverAddress = 'http://localhost:3000';
 
 describe('AxiomZen Music Recommendations Engine Test', function () {
-	var file, followDataFilePath, listenDataFilePath, followData, listenData;
-    var followCallbackCount = 0, listenCallbackCount = 0;
-
+	var file, listenObj, followData = [], listenData = [];
     var followDataFilePath = __dirname + '/data/follows.json';
     var listenDataFilePath = __dirname + '/data/listen.json';
 
-    before(function () {
+    before(function() {
         file = fs.readFileSync(followDataFilePath, 'utf8');
-        followData = JSON.parse(file);
+        followData = JSON.parse(file).operations;
 
         file = fs.readFileSync(listenDataFilePath, 'utf8');
-        listenData = JSON.parse(file);
+        listenObj = JSON.parse(file).userIds;
+
+        // convert listen data to an array of request bodies for simple iteration using async
+        for (var id in listenObj) {
+            if (listenObj.hasOwnProperty(id)) {
+                for (var i = 0; i < listenObj[id].length; i++)
+                    listenData.push({'user': id, 'music': listenObj[id][i]});
+            }
+        }
     });
 
     it('should not return any errors', function (done) {
+        async.parallel([feedFollowData, feedListenData], recommend);
+
         // feed follows.json to POST/follow endpoint
-
-        var operationsList = followData['operations'];
-        followCallbackCount = operationsList.length;
-
-        for (var i = 0; i < operationsList.length; i++) {
-            // parse each from and to user ids
-            var operation = operationsList[i];
-            var fromUserId = operation[0];
-            var toUserId = operation[1];
-            var body = {'from': fromUserId, 'to': toUserId};
-
-            request(serverAddress)
-            .post('/follow')
-            .send(body)
-            .expect(200)
-            .end(function (err, res) {
-                if (err) 
-                    return done(err);
-                followCallbackCount--;
-                if (followCallbackCount === 0) 
-                    recommend();
-          });
+        function feedFollowData(callback) {
+            async.each(followData, function (operation, followRequestDone) {
+                // parse from and to user ids
+                var fromUserId = operation[0];
+                var toUserId = operation[1];
+                var body = {'from': fromUserId, 'to': toUserId};
+            
+                request(serverAddress)
+                .post('/follow')
+                .send(body)
+                .expect(200)
+                .end(function (err, res) {
+                    if (err) followRequestDone(err);
+                    else followRequestDone(null);
+                });
+            }, callback);
         }
         
          //feed listen.json to POST/listen endpoint 
-         var userIds = listenData['userIds'];
-
-         for (var id in userIds) {
-            var musicList = userIds[id];
-            listenCallbackCount += musicList.length;
-            for (var i = 0; i < musicList.length; i++) {
-                var musicId = musicList[i];
-                var body = {'user': id, 'music': musicId};
-
+        function feedListenData(callback) {
+            async.each(listenData, function (body, listenRequestDone) {
                 request(serverAddress)
                 .post('/listen')
                 .send(body)
                 .expect(200)
                 .end(function (err, res) {
-                    if (err)
-                        return done(err);
-                    listenCallbackCount--;
-                    if (listenCallbackCount === 0) 
-                        recommend();
-                });
-            }
-        }
-        // GET/recommendations endpoint
-        function recommend() {
-            if (listenCallbackCount == 0 && listenCallbackCount == 0) { 
+                    if (err) listenRequestDone(err);
+                    else listenRequestDone(null);
 
-                request(serverAddress)
-                .get('/recommendations')
-                .query({'user': 'a'})
-                .expect(200)
-                .end(function (err, res) {
-                    if (err) return done(err);
-                    console.log('Recommended Music for userId "a":');
-                    console.log(JSON.stringify(res.body, null, 3));
-                    done();
                 });
-            }
+            }, callback);
+        }
+    
+        // GET/recommendations endpoint
+        function recommend(err, results) {
+            if (err) return done(err);
+            request(serverAddress)
+            .get('/recommendations')
+            .query({'user': 'a'})
+            .expect(200)
+            .end(function (err, res) {
+                if (err) return done(err);
+                console.log('Recommended Music for userId "a":');
+                console.log(JSON.stringify(res.body, null, 3));
+                done();
+            });
         }
     });
 });
